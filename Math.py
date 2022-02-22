2d29c33283b7c6305eeb75eb3807ca7b3b82b840
utkarshlanjewar <utkarsh_lanjewar@persistent.com>
2022-02-22 18:34:23 +0530

    Added fun headers

--- Math.py
+++ Math.py
@@ -1,12 +1,15 @@
-
+#Addd Implemetation
 def add(x,y):
     pass
 
+#Sub Implemetation
 def substract(x,y):
     pass
 
+#Multiply Implemetation
 def multiply(x,y):
     pass
 
+#Divide Implemetation
 def divide(x,y):
     pass 
